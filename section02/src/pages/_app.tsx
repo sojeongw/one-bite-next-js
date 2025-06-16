09a9c7912e2c2c9c559659f7e92f5b2c90bdeb68
sojeongw <sojeongw.official@gmail.com>
2025-06-16 15:28:50 +0900

    3.11. 네비게이팅

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,7 +1,29 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 // root 컴포넌트의 역할을 한다.
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push("/test");
+  };
+
+  return (
+    <>
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/search"}>search</Link>
+        &nbsp;
+        <Link href={"/book/1"}>book</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
