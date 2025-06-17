94d48dd29f00be6574b572b6b9865773a3f063c1
sojeongw <sojeongw.official@gmail.com>
2025-06-17 15:44:17 +0900

    3.12. 프리페칭

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 // root 컴포넌트의 역할을 한다.
 export default function App({ Component, pageProps }: AppProps) {
@@ -11,12 +12,19 @@ export default function App({ Component, pageProps }: AppProps) {
     router.push("/test");
   };
 
+  // router로 이동하면 프리페칭이 되지 않아 명시적으로 해줘야 한다.
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
+
   return (
     <>
       <header>
         <Link href={"/"}>index</Link>
         &nbsp;
-        <Link href={"/search"}>search</Link>
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>
         &nbsp;
         <Link href={"/book/1"}>book</Link>
         <div>
