3ab403506cb23d70fd47029164fe3a2ecc75e6ff
sojeongw <sojeongw.official@gmail.com>
2025-06-20 13:39:11 +0900

    3.8. 글로벌 레이아웃 설정하기

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,37 +1,12 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 // root 컴포넌트의 역할을 한다.
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  const onClickButton = () => {
-    router.push("/test");
-  };
-
-  // router로 이동하면 프리페칭이 되지 않아 명시적으로 해줘야 한다.
-  useEffect(() => {
-    router.prefetch("/test");
-  }, []);
-
   return (
-    <>
-      <header>
-        <Link href={"/"}>index</Link>
-        &nbsp;
-        <Link href={"/search"} prefetch={false}>
-          search
-        </Link>
-        &nbsp;
-        <Link href={"/book/1"}>book</Link>
-        <div>
-          <button onClick={onClickButton}>/test 페이지로 이동</button>
-        </div>
-      </header>
+    <GlobalLayout>
       <Component {...pageProps} />
-    </>
+    </GlobalLayout>
   );
 }
