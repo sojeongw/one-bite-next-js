5bf890f6109cd9770eaa8e641bd448e07269281a
sojeongw <sojeongw.official@gmail.com>
2025-06-23 14:05:20 +0900

    3.12. SSR 실습

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -4,19 +4,42 @@ import style from "./ index.module.css";
 import { ReactNode } from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
+// 컴포넌트보다 먼저 실행되어서 컴포넌트에서 필요한 데이터를 불러오는 함수
+// 사전 렌더링 하면서 서버에서 딱 한번만 실행된다. 브라우저에서 실행하지 않는다.
+export const getServerSideProps = async () => {
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+
+  return {
+    // props라는 하나의 프로퍼티 안에 있어야 한다.
+    props: {
+      allBooks,
+      recoBooks,
+    },
+  };
+};
+
+export default function Home({
+  allBooks,
+  recoBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
