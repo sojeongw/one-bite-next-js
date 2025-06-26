d1b662e75c07d15370170da1d703558833b772cc
sojeongw <sojeongw.official@gmail.com>
2025-06-26 13:41:20 +0900

    3.16. 폴백 옵션 설정하기

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,6 +1,7 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-book";
+import { useRouter } from "next/router";
 
 export const getStaticPaths = () => {
   return {
@@ -15,13 +16,19 @@ export const getStaticPaths = () => {
         params: { id: "3" },
       },
     ],
-    fallback: false,
+    fallback: true,
   };
 };
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
+
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
@@ -33,6 +40,9 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 export default function Page({
   book,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  if (router.isFallback) return "로딩중입니다";
   if (!book) {
     return "문제가 발생했습니다. 다시 시도하세요.";
   }
